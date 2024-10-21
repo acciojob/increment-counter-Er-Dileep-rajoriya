b345a111791f2383d931ed687f6c21347c9f8345
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 18:38:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let btn = document.getElementById("incrementBtn");
+
+btn.addEventListener("click", () => {
+  let count = document.getElementById("counter");
+
+  let counterNum = parseInt(count.innerText);
+
+  alert(counterNum);
+  count.innerText = counterNum + 1;
+});
